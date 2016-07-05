d0a704d406dff05f6603cfd719cf0c2195bac7d8
AveryPratt <apratt2@uoregon.edu>
2016-07-04 22:26:56 -0700

    Add files via upload

---
+++ RocketryWebApp/RocketryWebApp/RocketryWebApp/Calculator/DeleteRocket.cs
@@ -0,0 +1,47 @@
+﻿using KSP_Library;
+using KSP_Library.Rocketry;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.IO;
+using System.Configuration;
+
+namespace RocketryWebApp.Calculator
+{
+    public partial class CalculatorWebForm
+    {
+        //protected void UserRocketsGridView_Delete()
+        //{
+        //    int rowIndex = Convert.ToInt32(UserRocketsGridView.DataKeys[Convert.ToInt32(e.CommandArgument)].Value);
+        //    Response.Write("Delete");
+        //}
+
+        //protected void UserRocketsGridView_Edit()
+        //{
+        //    int rowIndex = Convert.ToInt32(UserRocketsGridView.DataKeys[Convert.ToInt32(e.CommandArgument)].Value);
+        //    Response.Write("Edit");
+        //}
+
+        protected void UserRocketsGridView_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand deleteRocketCommand = new SqlCommand("DELETE FROM Rockets WHERE KerbalID = " + 
+                    selectKerbalID(connectionString).ToString() + " AND RocketID = " +
+                    e.Values[0] + ";",
+                    connection);
+                connection.Open();
+                deleteRocketCommand.ExecuteNonQuery();
+            }
+            getUserRockets();
+        }
+
+        protected void UserRocketsGridView_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+
+        }
+    }
+}
